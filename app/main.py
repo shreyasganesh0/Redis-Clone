33eaf50a6dac51e5cdab23274c61d95fbca862b7
Shreyas Ganesh <vansi.ganeshs@gmail.com>
2024-10-24 21:19:54 -0400

    Add PING-PONG functionality as a hard coded response

--- app/main.py
+++ app/main.py
@@ -6,7 +6,19 @@ def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    
+    server_socket.listen(6379) # listen on the port
+
+    while True:
+        client_socket,address = server_socket.accept() # wait for client request and accept the connection
+        
+        data_input = client_socket.recv(1024) # read the data from the client
+
+        if not data_input:
+            break # client closed the connection
+
+        client_socket.send(f'+PONG\r\n') # reply to the client with pong (hardcoded for now)
+    client_socket.close()
 
 
 if __name__ == "__main__":
