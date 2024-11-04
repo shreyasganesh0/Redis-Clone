1175b42de131ed08ccc676a4f3ab3c5d1da1260e
Shreyas Ganesh <vansi.ganeshs@gmail.com>
2024-11-03 23:40:27 -0500

    Add looping logic using length encoding

--- app/rbdhandler.py
+++ app/rbdhandler.py
@@ -5,8 +5,8 @@ from .errorhandler.exceptions import InvalidRdbFileException, DynamicMethodNotFo
 
 class RdbHandler:
 
-    subsection_types_set: set = set(["FA","FB","FC","FD","FE","FF"])
-    subsections_values_dict: dict = {subsection for subsection in subsection_types_set}
+    subsection_types_set: set = set([250,251,252,253,254,255])
+    subsections_values_dict: dict = {subsection:"" for subsection in [250,251,252,253,254,255]}
     
     file_byte_data: bytes
     position_in_file: int = 0
@@ -17,14 +17,21 @@ class RdbHandler:
         """Reads bits from a byte, from most significant to least significant."""
 
     
-        byte = self.file_byte_data[self.position_in_file]
+        byte_val = self.file_byte_data[self.position_in_file]
+
+        print(byte_val,"byte", type(byte_val))
+
+        bits_list =[]
 
         if option_for_sub_byte_parsing:
-            for i in range(2, number_of_bits):
-                yield (byte << (i)) & 1
+            for i in range(6,-1,-1):
+                print("here bitspar")
+                bits_list.append( str((byte_val >> (i)) & 1))
         else:
-            for i in range(number_of_bits):
-                yield (byte << (i)) & 1
+            for i in range(8,6,-1):
+                bits_list.append( str((byte_val >> (i)) & 1))
+
+        return "".join(bits_list)
 
 
     def setrbd(self, parse_obj: argparse) -> None:
@@ -40,7 +47,7 @@ class RdbHandler:
         
         version = self.valid_file()
         
-        print("RDB version  number ", version)
+        print("RDB version number ", version)
 
         self.split_subsections()
 
@@ -89,47 +96,62 @@ class RdbHandler:
     
     def split_subsections(self) -> list:
 
-        byte_hex_val_as_string = "temp"
+        while self.position_in_file <len(self.file_byte_data):
         
-        while byte_hex_val_as_string != "FF":
-            
-            byte = self.file_byte_data[self.position_in_file]
-            
-            byte_hex_val_as_string = byte.hex()
-            
-            if byte_hex_val_as_string in self.subsection_types_set:
+            byte_val =self.file_byte_data[self.position_in_file]
 
-                try:
-                    generic_subsection_handler_method = getattr(self, byte_hex_val_as_string)
+            print(byte_val)
 
-                    generic_subsection_handler_method()
+            if byte_val in self.subsection_types_set:
+                current_header_for_data = byte_val
+                self.position_in_file+=1
+            
+            else:
+                
 
-                except AttributeError:
-                    raise DynamicMethodNotFoundException(self, byte_hex_val_as_string)
+                length_of_bytes = self.length_encoding_decode()
+
+                data = str(self.file_byte_data[self.position_in_file:self.position_in_file+length_of_bytes+1],'utf-8')
+
+                print("data ", data)
+
+                self.subsections_values_dict[current_header_for_data] += data+" "
+
+                self.position_in_file+=length_of_bytes+1
+
+                print(self.subsections_values_dict)
+
+
+
+
+
 
         return
 
     def length_encoding_decode(self):
         
-        first_two_bits_as_string = str(self.read_bits(number_of_bits = 2))
+        first_two_bits_as_string = self.read_bits(number_of_bits = 2)
 
-        print(first_two_bits_as_string)
+        print(first_two_bits_as_string, "first two bit")
 
         match first_two_bits_as_string:
 
             case "00":
-                length_of_data_in_bytes = int(str(self.read_bits(number_of_bits = 6, option_for_sub_byte_parsing = True)))
-                print(length_of_data_in_bytes)
+                print("in 00")
+                length_of_data_in_bytes = int(self.read_bits(number_of_bits = 6, option_for_sub_byte_parsing = True),2)
+                print(length_of_data_in_bytes,"length")
             
             case "01":
-                length_of_data_in_bytes = int(str(self.read_bits(number_of_bits= 6, option_for_sub_byte_parsing = True)))
+                length_of_data_in_bytes = int(self.read_bits(number_of_bits= 6, option_for_sub_byte_parsing = True))
                 self.position_in_file+= 1
                 length_of_data_in_bytes = int(str(self.read_bits(number_of_bits= 8)))
 
             case "10":
+                print("in 10")
                 self.position_in_file+= 1
+                string_of_bits_for_length_encoding=""
                 for i in range(4):
-                     string_of_bits_for_length_encoding = str(self.read_bits(number_of_bits= 8))
+                     string_of_bits_for_length_encoding += str(self.read_bits(number_of_bits= 8))
                 length_of_data_in_bytes =int(string_of_bits_for_length_encoding)
             
             case "11":
@@ -143,30 +165,22 @@ class RdbHandler:
                     
                     case 1:
                         self.position_in_file += 1
-                        length_of_integer_in_bits = 8
+                        length_of_integer_in_bits = 16
                     
                     case 2:
                         self.position_in_file += 1
-                        length_of_integer_in_bits = 8
+                        length_of_integer_in_bits = 32
 
                     case 3:
                         self.position_in_file += 1
                         #clen follows might have to call this method again a little too complex to tackle for now will come back to this
                         
 
-                    
-
-
-
+        return length_of_data_in_bytes
         first_two_bits_as_int = int(first_two_bits_as_string, 2)
 
-
-       
-
     
-    def fa(self):
-        self.position_in_file+=1
-        self.length_encoding_decode()
+
 
 
 
